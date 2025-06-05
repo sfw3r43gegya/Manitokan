1e9b7eceff2087e08b664fd14c8ec47ffe5eebf7
danemalenfant <dane.malenfant@mail.mcgill.ca>
2025-06-04 22:20:14 -0400

    jbk k

--- envs/__init__.py
+++ envs/__init__.py
@@ -1,17 +1,13 @@
 from functools import partial
 from .multiagentenv import MultiAgentEnv
-from .doorkey import DoorKeyEnv5x5
+
 
 from .iw2si import iw2si
-
-
-
 
 
 def env_fn(env, **kwargs) -> MultiAgentEnv:
     return env(**kwargs)
 
 REGISTRY = {}
-REGISTRY["dk"] = partial(env_fn, env=DoorKeyEnv5x5)
 REGISTRY["iw2si"] = partial(env_fn, env=iw2si)
 
